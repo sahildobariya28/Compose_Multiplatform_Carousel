043bec9c9279f06e11ad2646c61bef5ced8e12aa
mac <sahildobariya28@gmail.com>
2023-09-15 15:18:55 +0530

    Compose Multiplatform is Ready to Use.

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.library").version("8.0.1").apply(false)
+    kotlin("multiplatform").version("1.8.20").apply(false)
+    id("org.jetbrains.compose").version("1.4.0")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
