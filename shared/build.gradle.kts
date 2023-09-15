bb219c8edef81fd4bedba86f57c891c63964635a
mac <sahildobariya28@gmail.com>
2023-09-15 16:58:46 +0530

    Compose Multiplatform is Ready to Use.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -36,9 +36,28 @@ kotlin {
                 implementation(compose.components.resources)
             }
         }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+
+            }
+        }
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                
             }
         }
     }
